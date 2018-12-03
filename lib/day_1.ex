5635c69f2d7cdbb5252f8d69ef4ec68221c35a70
Tyler Wray <wraytw@gmail.com>
2018-12-02 22:20:39 -0700

    Almost got day 1 part 2...

--- lib/day_1.ex
+++ lib/day_1.ex
@@ -1,9 +1,11 @@
 defmodule AdventOfCode.Day1 do
   defp combine(x, acc) do
     operator = String.at(x, 0)
-    number = x
+
+    number =
+      x
       |> String.slice(1..-1)
-      |> String.to_integer
+      |> String.to_integer()
 
     case operator do
       "+" -> acc + number
@@ -11,11 +13,33 @@ defmodule AdventOfCode.Day1 do
     end
   end
 
-  def sum do
+  defp get_frequency_changes do
     "lib/day_1.txt"
-    |> Path.absname
-    |> File.read!
+    |> Path.absname()
+    |> File.read!()
     |> String.split("\n")
+  end
+
+  def sum do
+    get_frequency_changes()
     |> Enum.reduce(0, &combine/2)
+  end
+
+  defp hunt_duplicate(x, {map, sum}) do
+    frequency = combine(x, sum)
+
+    exists = Map.has_key?(map, frequency)
+
+    case exists do
+      false -> {:cont, {Map.put(map, frequency, 1), frequency}}
+      true -> {:halt, {map, frequency}}
+    end
+  end
+
+  def first_duplicate do
+    # TODO: Figure out how to continually go through the list of frequency changes
+    # Recursion?
+    get_frequency_changes()
+    |> Enum.reduce_while({%{}, 0}, &hunt_duplicate/2)
   end
 end
